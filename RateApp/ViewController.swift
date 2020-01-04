213fb99df82b0e1187b8782f943c8e64cfb266ad
sumit.mishra1 <sumit.mishra@zappfresh.com>
2020-01-04 11:30:22 +0530

    Initial Commit

---
+++ RateApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RateApp
+//
+//  Created by Zappfresh on 04/01/20.
+//  Copyright © 2020 Demo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
